128ade1858da41474dc81424223704c141fc569f
Nemanja Muncan <nemanja_muncan@yahoo.com>
2018-05-14 02:42:30 +0200

    added scroll functionality

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React, {Component} from 'react';
 import CardList from './CardList';
-import SearchBox from './SearchBox'
-import {robots} from './robots';
+import SearchBox from './SearchBox';
+import Scroll from './Scroll';
 import './App.css';
 
 
@@ -9,13 +9,23 @@ export class App extends Component {
 	constructor() {
 		super();
 		this.state= {
-				robots:robots,
+				robots:[],
 				searchfield:''
 		}
 	}
 
+	componentDidMount () {
+		fetch('https://jsonplaceholder.typicode.com/users')
+		.then(response => {
+			return response.json();
+		})
+		.then(users => {
+			this.setState({robots:users});
+		});
+	}
+
 	onSearchChange = (event) => {
-		this.setState({searchfield:event.target.value})
+		this.setState({searchfield:event.target.value});
 		
 	}
 
@@ -24,13 +34,22 @@ export class App extends Component {
 			return robots.name.toLowerCase().includes(this.state.searchfield.toLowerCase());
 		})
 
-		return (
-			<div className='tc'>
-				<h1 className='f1'>Robofriends</h1>
-				<SearchBox searchChange={this.onSearchChange}/>		
-				<CardList robots={filteredRobots} />
-			</div>
+		if(this.state.robots.length === 0) {
+			return <h1 className='tc'>LOADING...</h1>
+		}	else {
+			return (
+				<div className='tc'>
+					<h1 className='f1'>Robofriends</h1>
+					<SearchBox searchChange={this.onSearchChange}/>
+					<Scroll>		
+						<CardList robots={filteredRobots} />
+					</Scroll>
+				</div>
 		);
+
+		}
+
+
 	}
 	
 }
